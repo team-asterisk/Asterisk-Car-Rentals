a02c2cc7e15ad9fbb12c631e8b92f9b5452af2a8
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-07-22 17:49:59 +0300

    Create auth.router/controller.js provides information for user authorisation

---
+++ app/routers/auth.router/controller.js
@@ -0,0 +1,41 @@
+class CarRentalsController {
+    constructor(data) {
+        this.data = data;
+    }
+
+    getSignUpForm(req, res) {
+        return res.render('auth/register');
+    }
+    getSignInForm(req, res) {
+        return res.render('auth/login');
+    }
+    signOut(req, res) {
+        req.logout();
+        return res.redirect('/');
+    }
+
+    signUp(req, res) {
+        const bodyUser = req.body;
+
+        this.data.users.findByUsername(bodyUser.username)
+            .then((dbUser) => {
+                if (dbUser) {
+                    throw new Error('User already exists');
+                }
+
+                return this.data.users.create(bodyUser);
+            })
+            .then((dbUser) => {
+                return res.redirect('/auth/login');
+            })
+            .catch((err) => {
+                req.flash('error', err);
+            });
+    }
+}
+
+const init = (data) => {
+    return new CarRentalsController(data);
+};
+
+module.exports = { init };
