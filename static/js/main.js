d2467f0d2648f7e87278e1b4e08d169c0cf253af
zachdimitrov <zahari_d@abv.bg>
2017-07-23 20:00:23 +0300

    added form clear if wrong

--- static/js/main.js
+++ static/js/main.js
@@ -28,9 +28,11 @@ $(() => {
         .on('changeDate', (ev) => {
             if (ev.date.valueOf() < now.valueOf()) {
                 toastr.error("Today or future date is required!", "Attention!");
+                $(ev.target).val("");
             }
             if (endDate && (ev.date.valueOf() > endDate.valueOf())) {
                 toastr.error("Pick-up date must be before end date!", "Attention!");
+                $(ev.target).val("");
             }
             startDate = ev.date;
         });
@@ -39,9 +41,11 @@ $(() => {
         .on('changeDate', (ev) => {
             if (ev.date.valueOf() < now.valueOf()) {
                 toastr.error("Future date is required!", "Error!");
+                $(ev.target).val("");
             }
             if (startDate && (ev.date.valueOf() < startDate.valueOf())) {
                 toastr.error("End date must be after pickup date!", "Attention!");
+                $(ev.target).val("");
             }
             endDate = ev.date;
         });
