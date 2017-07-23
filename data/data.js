fd588b1397f92bf583c2f6fa6e6e48ebf52ea178
zachdimitrov <zahari_d@abv.bg>
2017-07-23 14:26:46 +0300

    removed Deals and Categories and updated data.js

--- data/data.js
+++ data/data.js
@@ -1,6 +1,4 @@
 const CarsData = require('./models.data/cars.data');
-const CarCategoriesData = require('./models.data/carCategories.data');
-const DealsData = require('./models.data/deals.data');
 const ReviewsData = require('./models.data/reviews.data');
 const CommentsData = require('./models.data/comments.data');
 const UsersData = require('./models.data/users.data');
@@ -8,8 +6,6 @@ const UsersData = require('./models.data/users.data');
 const init = (db) => {
     return Promise.resolve({
         cars: new CarsData(db),
-        carCategories: new CarCategoriesData(db),
-        deals: new DealsData(db),
         reviews: new ReviewsData(db),
         comments: new CommentsData(db),
         users: new UsersData(db),
