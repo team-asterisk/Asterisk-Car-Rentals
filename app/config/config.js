e42552d82a97ac101d3517f46982efa22c12979e
Emil Dimitrov <emil_132@abv.bg>
2017-07-23 17:24:23 +0300

    added static paths to css, js and images

--- app/config/config.js
+++ app/config/config.js
@@ -17,6 +17,10 @@ const applyTo = (app) => {
     const staticsPath = path.join(__dirname, '../../static');
     app.use('/static', express.static(staticsPath));
 
+    app.use('/css', express.static(path.join(__dirname, '../../static/css/')));
+    app.use('/js', express.static(path.join(__dirname, '../../static/js/')));
+    app.use('/images', express.static(path.join(__dirname, '../../static/images/')));
+
     app.use(cookieParser('keyboard cat'));
 };
 
