0a265b44221869034bc7c94036bf18e18489a64b
tinmanjk <tinmanjk@mail.com>
2017-07-24 10:07:14 +0300

    removing example

--- server.js
+++ server.js
@@ -1,34 +1,17 @@
 /* eslint-disable no-console */
-/*global to*/
-
-const async = () => {
-    return Promise.resolve();
-};
-
-require('./utils/toAwait');
-
 
 const config = require('./config');
 
 (async () => {
 
-    //TO example without try catch
-    let db = await to(require('./db').init(config.connectionString));
-    if (db.err) {
-        console.log(db.err.message);
-    } else {
-        db = db.data;
-    }
-
     try {
-
+        const db = await (require('./db').init(config.connectionString));
         const data = await require('./data').init(db);
         const app = await require('./app').init(data);
 
         app.listen(config.port, () =>
             console.log(`Car Rentals is now live at http://localhost:${config.port}`));
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err.message);
     }
 })();
