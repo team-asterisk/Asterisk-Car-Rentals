dfb8c72b86c7bb3692bdd334de7b04f4c8cef68f
tinmanjk <tinmanjk@mail.com>
2017-07-24 07:05:00 +0300

    server js with async-await

--- server.js
+++ server.js
@@ -6,14 +6,28 @@ const async = () => {
 
 const config = require('./config');
 
-async()
-.then(() => require('./db').init(config.connectionString))
-    .then((db) => require('./data').init(db))
-    .then((data) => require('./app').init(data))
-    .then((app) => {
+(async () => {
+    try {
+        const db = await require('./db').init(config.connectionString);
+        const data = await require('./data').init(db);
+        const app = await require('./app').init(data);
+
         app.listen(config.port, () =>
             console.log(`Car Rentals is now live at http://localhost:${config.port}`));
-    })
-    .catch((err) => {
+    }
+    catch (err) {
         console.log(err);
-    });
+    }
+})();
+
+// async()
+//     .then(() => require('./db').init(config.connectionString))
+//     .then((db) => require('./data').init(db))
+//     .then((data) => require('./app').init(data))
+//     .then((app) => {
+//         app.listen(config.port, () =>
+//             console.log(`Car Rentals is now live at http://localhost:${config.port}`));
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
