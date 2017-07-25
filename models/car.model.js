0686d0f2c12ccdaa4b9fbde3180fdb396b53f573
Emil Dimitrov <emil_132@abv.bg>
2017-07-26 00:30:13 +0300

    better validations

--- models/car.model.js
+++ models/car.model.js
@@ -3,21 +3,21 @@ const size = require('../utils').constants.size;
 const convert = require('../utils/inputConverter').convert;
 
 class Car {
-    constructor(model) {
-        this.makemodel = model.makemodel;
-        this.category = model.category;
-        this.carphoto = model.carphoto;
-        this.adultscount = model.adultscount;
-        this.bagscount = model.bagscount;
-        this.doorscount = model.doorscount;
-        this.fueltype = model.fueltype;
-        this.transmission = model.transmission;
-        this.mileage = model.mileage;
-        this.yearofmanufacture = model.yearofmanufacture;
-        this.airconditioner = model.airconditioner;
-        this.baseprice = model.baseprice;
-        this.specialprice = model.specialprice;
-        this.specialpriceactivated = model.specialpriceactivated;
+    constructor(model, photoLink) {
+        this._makemodel = model.makemodel;
+        this._category = model.category;
+        this._carphotolink = photoLink;
+        this._adultscount = model.adultscount;
+        this._bagscount = model.bagscount;
+        this._doorscount = model.doorscount;
+        this._fueltype = model.fueltype;
+        this._transmission = model.transmission;
+        this._mileage = model.mileage;
+        this._yearofmanufacture = model.yearofmanufacture;
+        this._airconditioner = model.airconditioner;
+        this._baseprice = model.baseprice;
+        this._specialprice = model.specialprice;
+        this._specialpriceactivated = model.specialpriceactivated;
         this.comments = [];
         this.booked = [];
     }
@@ -26,172 +26,192 @@ class Car {
         return this._id;
     }
 
-    get makemodel() {
-        return this._makemodel;
+    get _makemodel() {
+        return this.makemodel;
     }
 
-    set makemodel(value) {
+    set _makemodel(value) {
         const name = convert(value);
         if (validator.validateString(name, size.MIN_NAME, size.MAX_NAME)) {
-            this._makemodel = name;
+            this.makemodel = name;
         } else {
             throw new Error('Invalid car make and model');
         }
     }
 
-    get category() {
-        return this._category;
+    get _category() {
+        return this.category;
     }
 
-    set category(value) {
+    set _category(value) {
         const name = convert(value);
         if (validator.validateString(name, size.MIN_NAME, size.MAX_NAME)) {
-            this._category = name;
+            this.category = name;
         } else {
             throw new Error('Invalid category name');
         }
     }
 
-    get carphoto() {
-        return this._carphoto;
+    get _carphotolink() {
+        return this.carphotolink;
     }
 
-    set carphoto(value) {
-        this._carphoto = value;
-    }
-
-    get adultscount() {
-        return this._adultscount;
-    }
-
-    set adultscount(value) {
-        const count = parseInt(value, 10);
-        if (typeof count !== 'number' || (count < 2 && count > 20)) {
-            throw new Error('Invalid adultscount');
+    set _carphotolink(value) {
+        const link = convert(value);
+        if (validator.validateImageExtension(link)) {
+            this.carphotolink = link;
+        } else {
+            throw new Error('Invalid photo link - only jpg or png');
         }
-        this._adultscount = count;
     }
 
-    get bagscount() {
-        return this._bagscount;
+    get _adultscount() {
+        return this.adultscount;
     }
 
-    set bagscount(value) {
+    set _adultscount(value) {
         const count = parseInt(value, 10);
-        if (typeof count !== 'number' || (count < 2 && count > 20)) {
-            throw new Error('Invalid bagscount');
+        if (Number.isNaN(Number(count)) || (count < 2 || count > 20)) {
+            throw new Error('Invalid adultscount - must be in range 2-20');
         }
-        this._bagscount = count;
+
+        this.adultscount = count;
     }
 
-    get doorscount() {
-        return this._doorscount;
+    get _bagscount() {
+        return this.bagscount;
     }
 
-    set doorscount(value) {
+    set _bagscount(value) {
         const count = parseInt(value, 10);
-        if (typeof count !== 'number' || (count < 2 && count > 5)) {
-            throw new Error('Invalid doorscount');
+        if (Number.isNaN(Number(count)) || (count < 2 || count > 20)) {
+            throw new Error('Invalid bagscount - must be in range 2-20');
         }
-        this._doorscount = count;
+
+        this.bagscount = count;
     }
 
-    get fueltype() {
-        return this._fueltype;
+    get _doorscount() {
+        return this.doorscount;
     }
 
-    set fueltype(value) {
+    set _doorscount(value) {
+        const count = parseInt(value, 10);
+        if (Number.isNaN(Number(count)) || (count < 2 || count > 5)) {
+            throw new Error('Invalid doorscount - must be in range 2-5');
+        }
+
+        this.doorscount = count;
+    }
+
+    get _fueltype() {
+        return this.fueltype;
+    }
+
+    set _fueltype(value) {
         const fuelType = convert(value);
         if (validator.validateString(fuelType, size.MIN_NAME, size.MAX_NAME)) {
-            this._fueltype = fuelType;
+            this.fueltype = fuelType;
         } else {
             throw new Error('Invalid fuel type name');
         }
     }
 
-    get transmission() {
-        return this._transmission;
+    get _transmission() {
+        return this.transmission;
     }
 
-    set transmission(value) {
+    set _transmission(value) {
         const transmissionType = convert(value);
         if (validator.validateString(transmissionType, size.MIN_NAME, size.MAX_NAME)) {
-            this._transmission = transmissionType;
+            this.transmission = transmissionType;
         } else {
             throw new Error('Invalid transmission type');
         }
     }
 
-    get mileage() {
-        return this._mileage;
+    get _mileage() {
+        return this.mileage;
     }
 
-    set mileage(value) {
+    set _mileage(value) {
         const mpg = parseInt(value, 10);
-        if (typeof mpg !== 'number' || (mpg < 1 && mpg > 200)) {
-            throw new Error('Invalid mileage');
+        if (Number.isNaN(Number(mpg)) || (mpg < 1 || mpg > 200)) {
+            throw new Error('Invalid mileage - must be in range 1-200');
         }
-        this._mileage = mpg;
+
+        this.mileage = mpg;
     }
 
-    get yearofmanufacture() {
-        return this._yearofmanufacture;
+    get _yearofmanufacture() {
+        return this.yearofmanufacture;
     }
 
-    set yearofmanufacture(value) {
+    set _yearofmanufacture(value) {
         const year = parseInt(value, 10);
-        if (typeof year !== 'number' || (year < 2010 && year > 2020)) {
-            throw new Error('Invalid year of manufacture');
+        if (Number.isNaN(Number(year)) || (year < 2010 || year > 2020)) {
+            throw new Error('Invalid year of manufacture - must be in range 2010-2020');
         }
-        this._yearofmanufacture = year;
+
+        this.yearofmanufacture = year;
     }
 
-    get airconditioner() {
-        return this._airconditioner;
+    get _airconditioner() {
+        return this.airconditioner;
     }
 
-    set airconditioner(value) {
-        const ac = parseInt(value, 10);
-        if (typeof ac !== 'number' || (ac < 0 && ac > 1)) {
+    set _airconditioner(value) {
+        if (typeof value === 'undefined') {
+            value = 0;
+        }
+
+        if (value < 0 || value > 1) {
             throw new Error('Invalid feature air conditioner');
         }
-        this._airconditioner = ac;
+
+        this.airconditioner = value;
     }
 
-    get baseprice() {
-        return this._baseprice;
+    get _baseprice() {
+        return this.baseprice;
     }
 
-    set baseprice(value) {
+    set _baseprice(value) {
         const price = parseFloat(value, 10);
-        if (typeof price !== 'number' || (price < 0 && price > 2000)) {
-            throw new Error('Invalid base price');
+        if (Number.isNaN(Number(price)) || (price < 0 || price > 2000)) {
+            throw new Error('Invalid base price - must be in range 0-2000');
         }
-        this._baseprice = price;
+
+        this.baseprice = price;
     }
 
-    get specialprice() {
-        return this._specialprice;
+    get _specialprice() {
+        return this.specialprice;
     }
 
-    set specialprice(value) {
+    set _specialprice(value) {
         const price = parseFloat(value, 10);
-        if (typeof price !== 'number' || (price < 0 && price > 2000)) {
-            throw new Error('Invalid special price');
+        if (Number.isNaN(Number(price)) || (price < 0 || price > 2000)) {
+            throw new Error('Invalid special price - must be in range 0-2000');
         }
-        this._specialprice = price;
+
+        this.specialprice = price;
     }
 
-    get specialpriceactivated() {
-        return this._specialpriceactivated;
+    get _specialpriceactivated() {
+        return this.specialpriceactivated;
     }
 
-    set specialpriceactivated(value) {
-        const special = parseInt(value, 10);
-        if (typeof special !== 'number' || (special < 0 && special > 1)) {
-            throw new Error('Invalid feature special price activated');
+    set _specialpriceactivated(value) {
+        if (typeof value === 'undefined') {
+            value = 0;
         }
-        this._specialpriceactivated = special;
+        
+        if (value < 0 || value > 1) {
+            throw new Error('Invalid feature air conditioner');
+        }
+
+        this.specialpriceactivated = value;
     }
 }
 
