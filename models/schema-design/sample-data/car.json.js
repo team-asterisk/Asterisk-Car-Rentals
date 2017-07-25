94b87a237514032bff6692306fd0d3b006cf31b0
tinmanjk <tinmanjk@mail.com>
2017-07-25 09:21:06 +0300

    added carmodel model to schema design

--- models/schema-design/sample-data/car.json.js
+++ models/schema-design/sample-data/car.json.js
@@ -1,16 +1,7 @@
 const car = {
-    'manufacturer': 'Mercedes Benz',
-    'model': 'SL500',
-    'reviews': [
-        {
-            'user': []
-        }
-    ],
-    //what about?
-    'comments': [
-        {
-            'user': []
-        }
-    ]
+    'id': '1',
+    'carmodel_id': 3,
+    'currentLocation': 'Sofia'
 };
+
 module.exports = car;
---
+++ models/schema-design/sample-data/carmodel.json.js
@@ -0,0 +1,20 @@
+// specific model-year of a car 
+// to be added as a new car in the portfolio
+// cars 
+const carModel = {
+    'id': 3,
+    'manufacturer': 'Mercedes Benz',
+    'model': 'SL500',
+    'reviews': [
+        {
+            'user': []
+        }
+    ],
+    //what about?
+    'comments': [
+        {
+            'user': []
+        }
+    ]
+};
+module.exports = carModel;
