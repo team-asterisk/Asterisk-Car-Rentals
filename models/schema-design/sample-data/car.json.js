2c6a14dd24cba90042a47825b1dad53a3900e452
tinmanjk <tinmanjk@mail.com>
2017-07-25 08:04:40 +0300

    changed json to js to allow for comments in json

---
+++ models/schema-design/sample-data/car.json.js
@@ -0,0 +1,16 @@
+const car = {
+    'manufacturer': 'Mercedes Benz',
+    'model': 'SL500',
+    'reviews': [
+        {
+            'user': []
+        }
+    ],
+    //what about?
+    'comments': [
+        {
+            'user': []
+        }
+    ]
+};
+module.exports = car;
