61651040372879ab194b76f1b15dd18e202bf214
zachdimitrov <zach_dimitrov@yahoo.com>
2017-07-27 11:43:03 +0300

    corrected tabl;e view for my bookings

--- data/data.js
+++ data/data.js
@@ -1,13 +1,11 @@
 const CarsData = require('./models.data/cars.data');
 const ReviewsData = require('./models.data/reviews.data');
-const CommentsData = require('./models.data/comments.data');
 const UsersData = require('./models.data/users.data');
 
 const init = (db) => {
     return Promise.resolve({
         cars: new CarsData(db),
         reviews: new ReviewsData(db),
-        comments: new CommentsData(db),
         users: new UsersData(db),
     });
 };
