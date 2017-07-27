3552c81f4c12a1fb912c1ff5d7dc500892745ae9
Emil Dimitrov <emil_132@abv.bg>
2017-07-27 08:44:53 +0300

    added edituser functionality

--- app/routers/admin.router/controller.js
+++ app/routers/admin.router/controller.js
@@ -37,6 +37,44 @@ class AdminController {
                 );
             });
     }
+
+    getEditUserByIdForm(req, res) {
+        Promise.resolve(this.data.users.findById(req.params.id))
+            .then((user) => {
+                return res.render('auth/admin/edituser', {
+                        user,
+                    req: req,
+                });
+            });
+    }
+
+    updateUserProfile(req, res) {
+        const bodyUser = req.body;
+        return this._updateUserProperties(req.params.id, bodyUser)
+            .then((user) => {
+                return this.data.users.updateById(user);
+            })
+            .then(() => {
+                return res.redirect('/auth/viewusers');
+            })
+            .catch((err) => {
+                req.toastr.error(err.message);
+                return res.redirect('/auth/viewusers');
+            });
+    }
+
+    _updateUserProperties(userId, user) {
+        return this.data.users.findById(userId)
+            .then((dbUser) => {
+                user._id = dbUser._id;
+                user.username = dbUser.username;
+                user.role = dbUser.role;
+                user.bookings = dbUser.bookings;
+                user.passHash = dbUser.passHash;
+
+                return Promise.resolve(user);
+            });
+    }
 }
 
 const init = (data) => {
