77a9ce2f0b0883937cb30806c2e23a753c653f62
Emil Dimitrov <emil_132@abv.bg>
2017-07-27 07:26:36 +0300

    extracted into admin router

--- app/routers/auth.router/router.js
+++ app/routers/auth.router/router.js
@@ -18,18 +18,6 @@ const attachTo = (app, data) => {
         .get('/bookings', (req, res) => {
             return controller.getMyBookings(req, res);
         })
-        .get('/viewbookings', (req, res) => {
-            return controller.getViewBookings(req, res);
-        })
-        .get('/viewcars', (req, res) => {
-            return controller.getViewCars(req, res);
-        })
-        .get('/viewusers', (req, res) => {
-            return controller.getViewUsers(req, res);
-        })
-        .get('/viewdeals', (req, res) => {
-            return controller.getViewDeals(req, res);
-        })
         .post('/profile', (req, res) => {
             return controller.updateProfile(req, res);
         })
