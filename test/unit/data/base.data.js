201d219767f7ba4e15baa13fb53e1f9e340b8d2a
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-07-29 07:56:33 +0300

    combined base.data tests in one file describes will separate tests - better readability

--- test/unit/data/base.data.js
+++ test/unit/data/base.data.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 
-const BaseData = require('../../../../data/base/base.data');
+const BaseData = require('../../../data/base/base.data');
 
 describe('when there are items in db', () => {
     const db = { collection: () => {} };
