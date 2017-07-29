a89c2991b5d5c8318d1ee08716ed2d64b09dde32
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-07-29 14:53:15 +0300

    added cars.data.create test

---
+++ test/unit/data/cars.data.tests.js
@@ -0,0 +1,43 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const rewire = require('rewire');
+const CarsData = rewire('../../../data/models.data/cars.data');
+
+describe('when there are items in db', () => {
+    // base.data parameters
+    const db = { collection: () => {} };
+    let data = null;
+
+    describe('Cars Data create(model)', () => {
+        const car = {};
+        const link = '';
+        const instance = { one: 1, two: 2, three: 3 };
+        const insert = () => {
+            return Promise.resolve(instance);
+        };
+
+        const newCarMock = () => {
+            return instance;
+        };
+
+        beforeEach(() => {
+            sinon.stub(db, 'collection').callsFake(() => {
+                return { insert };
+            });
+
+            CarsData.__set__('initCar', newCarMock);
+            data = new CarsData(db);
+        });
+
+        afterEach(() => {
+            db.collection.restore();
+        });
+
+        it('expect to return model', () => {
+            return data.create(car, link)
+                .then((models) => {
+                    expect(models).to.deep.equal(instance);
+                });
+        });
+    });
+});
