918f0cb5e4940ce11832c373e3eda9f7e53c3e06
Emil Dimitrov <emil_132@abv.bg>
2017-07-30 20:42:14 +0300

    added functional tests for login

--- test/browser/login.js
+++ test/browser/login.js
@@ -2,7 +2,10 @@
 const { expect } = require('chai');
 const { setupDriver } = require('./utils/setup-driver');
 const webdriver = require('selenium-webdriver');
-const { appUrl } = require('./config');
+const { appUrl, connectionString, port } = require('./config');
+
+let server = null;
+const { Server } = require('./../../server');
 
 describe('Login routes', () => {
     let driver = null;
@@ -11,8 +14,32 @@ describe('Login routes', () => {
     //     new webdriver.Builder()
     //         .build();
 
+    before(() => {
+        server = new Server();
+
+        return server.run(
+            {
+                connectionString: connectionString,
+                port: port,
+            })
+            .then(() => {
+                Promise.resolve();
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    });
+
+    after(() => {
+        return server.stop('drop database');
+    });
+
     beforeEach(() => {
         driver = setupDriver('chrome');
+    });
+
+    afterEach(function() {
+        driver.quit();
     });
 
     it('expect title with text "Car Rentals - Login"', (done) => {
@@ -22,6 +49,22 @@ describe('Login routes', () => {
             })
             .then((title) => {
                 expect(title).to.contain('Login');
+                done();
+            });
+    });
+
+    it('expect h1 with text "Login"', (done) => {
+        driver.get(appUrl + '/auth/login')
+            .then(() => {
+                return driver.findElement(
+                    webdriver.By.css('h1')
+                );
+            })
+            .then((el) => {
+                return el.getText();
+            })
+            .then((text) => {
+                expect(text).to.contain('Login');
                 done();
             });
     });
