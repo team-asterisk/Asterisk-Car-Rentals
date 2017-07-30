9c0a1e7ed8b3e916d36dc985e0eb985a3bd1684c
tinmanjk <tinmanjk@mail.com>
2017-07-30 15:05:31 +0300

    refactored server.js to give run and stop server promises for functional tests

--- server/server.js
+++ server/server.js
@@ -3,27 +3,69 @@ const async = () => {
     return Promise.resolve();
 };
 
-const run = (config) =>
-    async()
-        .then(() => require('./../db').init(config.connectionString))
-        .then((db) => require('./../data').init(db))
-        .then((data) => require('./../app').init(data))
-        .then((app) => {
-            //neded for WebSockets
-            const httpServer = app.listen(config.port, () =>
-                console.log(`Car Rentals is now live at http://localhost:${config.port}`));
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+class Server {
+
+    constructor() {
+        this.servers = [];
+    }
+
+    run(config) {
+        return async()
+            .then(() => require('./../db').init(config.connectionString))
+            .then((db) => require('./../data').init(db))
+            .then((data) => require('./../app').init(data))
+            .then((app) => {
+                //neded for WebSockets
+                const httpServer = app.listen(config.port, () =>
+                    console.log(`Car Rentals is now live at http://localhost:${config.port}`));
+                this.servers.push({
+                    config,
+                    httpServer
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    stop(options) {
+        return async()
+            .then(() => {
+                const server = this.servers.find((s) =>
+                    s.config.connectionString === options.config.connectionString &&
+                    s.config.port === options.config.port).httpServer;
+                if (!server) {
+                    return Promise.reject('Server not iniated');
+                }
+                return Promise.resolve(server);
+            })
+            .then((server) => {
+                return server.close(() => {
+                    console.log(`Server at: ${options.config.port} now closed `);
+                });
+            })
+            .then(() => {
+                if (options.config.connectionString) {
+                    const { MongoClient } = require('mongodb');
+                    return MongoClient.connect(options.config.connectionString)
+                        .then((db) => {
+                            return db.dropDatabase();
+                        })
+                        .then(() => {
+                            console.log(`Database: ${options.config.connectionString} sucessfully dropped`);
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        });
+
+                }
+                return Promise.resolve();
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+}
 
 
-// const {MongoClient} = require()
-
-// const stop = (config) => {
-//     async()
-
-// };
-
-
-module.exports = { run };
+module.exports = { Server };
