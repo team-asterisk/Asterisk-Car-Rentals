a9d751ecc7dc802588f7bb8c9c44171bd1c1edcb
tinmanjk <tinmanjk@mail.com>
2017-08-01 01:25:37 +0300

    changed master pug logic to include sockets just for master

--- static/js/sockets.js
+++ static/js/sockets.js
@@ -1,21 +1,6 @@
 //PORTA E MNOGO VAJEN
 var socket = io.connect('http://localhost:3001');
-// -> v tozi moment servera si slaga event listeneri
 
-
-//tova realno e eventa za koito slusha servera
-// socket.emit('message', {
-//     text: "Hello it finally works"
-// });
-
-// socket.on('to client', (data) => {
-//     //pravim neshto s datata
-//     console.log("Message received");
-//     console.log(data);
-// });
-
-//tova prosto trqbva da go ima samo na edno mqsto
-// ili na vsichki kadeto e administratora?
 socket.on('user logged out', (data) => {
     // toastr.error('To all clients', 'Attention!');
     toastr.info('User has book', 'Booking added',
@@ -31,12 +16,5 @@ socket.on('user logged out', (data) => {
             'extendedTimeOut': '1000',
             onclick: function () { window.location.assign("http://localhost:3001/auth/viewbookings"); }
         })
-    // console.log("To all clients");
-    // console.log(data);
 
 });
-
-// socket.on('news', function (data) {
-//     console.log(data);
-//     socket.emit('my other event', { my: 'data' });
-// });
